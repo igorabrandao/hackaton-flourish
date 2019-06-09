71f86fc47cb688e99073b10440e973cef67893ee
Igor Brandão <igorabrandao@gmail.com>
2019-06-09 12:50:15 -0300

    Inclusão dos arquivos

---
+++ web/config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "igora790";
+// MySQL Database Password
+$sql_db_pass = "Yuyuhakusho091518";
+// MySQL Database Name
+$sql_db_name = "igora790_hackaton";
+
+// Site URL
+$site_url = "http://hackaton.igorabrandao.com.br"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "testexpto"; // Your purchase code, don't give it to anyone. 
+?>
